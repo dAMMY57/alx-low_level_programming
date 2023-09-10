b262e81c5ed4f3baa45a97230d0feb7dfe594bf5
Odeyemi Damilola <pismart001@gmail.com>
2023-09-09 17:32:02 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*
+ *file: 4-Puts.c
+ */
+#include <stdio.h>
+
+/**
+ *main- Print "Programming is like building a multilingual puzzle"
+ *
+ *Always return 0.
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
+}
