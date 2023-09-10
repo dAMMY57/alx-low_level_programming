8147b2bb6dfe4cd7051121c312af24e1a881df0f
Odeyemi Damilola <pismart001@gmail.com>
2023-09-10 03:38:16 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/* File :5-printf.c */
+
+#include <stdio.h>
+
+/*
+ *main: print "with proper grammar, but the outcome is a piece of art,"
+ *followed by a new line
+ */
+int main(void)
+{
+  printf("\"with proper grammar, but the outcome is a piece of art,");
+
+  return(0);
+}
