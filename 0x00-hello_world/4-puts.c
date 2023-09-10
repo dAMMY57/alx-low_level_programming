21d073f5480b38350dca40a2325aa7f404a3f595
Odeyemi Damilola <pismart001@gmail.com>
2023-09-09 17:39:08 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,10 +9,8 @@
  *Always return 0.
  */
 int main(void)
-{
+{  
+         puts("\"Programming is like building a multilingual puzzle");
   
-  puts("\"Programming is like building a multilingual puzzle");
-
-  
-  return (0);
+         return (0);
 }
