f988e18495b3f2599dd406b0b184b5ef831a36f2
Odeyemi Damilola <pismart001@gmail.com>
2023-09-18 04:03:05 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints lowercase x10, followed by new line 
+ * print_alphabet_x10 - Prints lowercase x10, followed by new line
  */
 void print_alphabet_x10(void)
 {
