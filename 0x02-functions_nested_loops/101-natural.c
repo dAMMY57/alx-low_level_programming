c3d9db5641f5c562f9d2eeb654ee76357e3a8fe5
Odeyemi Damilola <pismart001@gmail.com>
2023-09-19 10:43:34 -0700

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,6 +2,9 @@
 /**
 *main - ENTRY POINT OF MY CODE
 *
+* Description: This program calculates and prints the sum
+* of all multiples of 3 or 5 below 1024 (excluded).
+*
 *Return- Always 0(success)
 */
 int main(void)
