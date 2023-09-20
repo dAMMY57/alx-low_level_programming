e5f48dc07b3def4c12494374a12415ade99b6d96
Odeyemi Damilola <pismart001@gmail.com>
2023-09-20 04:15:53 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,20 +18,20 @@ for (digit2 = digit1 + 1; digit2 <= 8; digit2++)
 {
 for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
 {
-putchar(digit1 + '0');   // Print the first digit
-putchar(digit2 + '0');   // Print the second digit
-putchar(digit3 + '0');   // Print the third digit
+putchar(digit1 + '0');   /* Print the first digit */
+putchar(digit2 + '0');   /* Print the second digit */
+putchar(digit3 + '0');   /* Print the third digit */
 
 if (digit1 != 7 || digit2 != 8 || digit3 != 9)
 {
-putchar(',');  // Print comma
-putchar(' ');  // Print space
+putchar(',');  /* Print comma */
+putchar(' ');  /* Print space */
 }
 }
 }
 }
 
-putchar('\n'); // Print newline character
+ putchar('\n'); /* Print newline character */
 
-return 0; // Return 0 to indicate successful execution
+ return 0; /* Return 0 to indicate successful execution */
 }
