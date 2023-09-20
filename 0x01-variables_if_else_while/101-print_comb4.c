ca5a946e5777310e686e548ad6c607595c73064e
Odeyemi Damilola <pismart001@gmail.com>
2023-09-20 04:10:22 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,39 +1,37 @@
-/*
- * File: 101-print_comb4.c
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of three different digits,
- *        in ascending order, separated by a comma followed by a space.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Description: Prints all possible different combinations of three digits in
+ * ascending order, separated by a comma and a space.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int digit1, digit2, digit3;
+int digit1, digit2, digit3;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+for (digit1 = 0; digit1 <= 7; digit1++)
+{
+for (digit2 = digit1 + 1; digit2 <= 8; digit2++)
+{
+for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
+{
+putchar(digit1 + '0');   // Print the first digit
+putchar(digit2 + '0');   // Print the second digit
+putchar(digit3 + '0');   // Print the third digit
 
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+{
+putchar(',');  // Print comma
+putchar(' ');  // Print space
+}
+}
+}
+}
 
-	putchar('\n');
+putchar('\n'); // Print newline character
 
-	return (0);
+return 0; // Return 0 to indicate successful execution
 }
