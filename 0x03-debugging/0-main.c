33f0f4344ba521107f7409e0f8a728c80ff01a51
Odeyemi Damilola <pismart001@gmail.com>
2023-09-20 07:44:39 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is 
- *positive, negative, or zero
- * 
+ * main - tests function that prints if integer is positive, negative, or zero
+ *
  *Return: Always 0
  */
-
 int main(void)
 {
 int i;
