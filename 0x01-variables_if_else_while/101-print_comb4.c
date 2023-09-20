7fd85f64af5b2095c26936b93bf75bf3e77c23ca
Odeyemi Damilola <pismart001@gmail.com>
2023-09-20 04:18:40 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -33,5 +33,5 @@ putchar(' ');  /* Print space */
 
 putchar('\n'); /* Print newline character */
 
- return (0); /* Return 0 to indicate successful execution */
+return (0); /* Return 0 to indicate successful execution */
 }
