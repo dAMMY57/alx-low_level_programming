37b67f27489e5d931c01191f849ce14aaf62d555
Odeyemi Damilola <pismart001@gmail.com>
2023-09-21 07:31:22 -0700

    Leap Year

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,14 +12,12 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-
-/*
+ /*
  *while (i < 10)
  *{
  *putchar(i); commented out the causing loop infinite
  *}
-*/
-
+ */
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
