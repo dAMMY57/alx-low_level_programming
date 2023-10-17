0706567d3353b177e4881c6442ca4cfbdfe0f4a0
Odeyemi Damilola <pismart001@gmail.com>
2023-10-17 08:50:40 -0700

    You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - print a square, followed by a new line
+ *by using _putchar function to print
+ *@size: where size is size of the square, if size is 0 or less
+ *the function should print only a new line
+ *use the # character to print the square.
+ */
+void print_square(int size)
+{
+int x,y;
+if(size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (x = 1; x <= size; x++)
+{
+for (y =1; y <= size; y++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
