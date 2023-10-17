f98aca34bf48023cdb232848bfa96009e5df1192
Odeyemi Damilola <pismart001@gmail.com>
2023-10-17 09:03:32 -0700

    You are so much sunshine in every square inch mandatory

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,15 +8,15 @@
  */
 void print_square(int size)
 {
-int x,y;
-if(size <= 0)
+int x, y;
+if (size <= 0)
 {
 _putchar('\n');
 return;
 }
 for (x = 1; x <= size; x++)
 {
-for (y =1; y <= size; y++)
+for (y = 1; y <= size; y++)
 {
 _putchar('#');
 }
