a51dc536e0987ec9382df6f44ca8d99782de1030
Odeyemi Damilola <pismart001@gmail.com>
2023-10-23 15:57:55 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+/**
+ * string_toupper - Change all lowercase letters of a string to uppercase
+ * @str: The input string
+ *
+ * Return: A pointer to the modified string
+ */
+char *string_toupper(char *str)
+{
+char *ptr = str;
+
+while (*str)
+{
+if (*str >= 'a' && *str <= 'z')
+*str = *str - 32;
+str++;
+}
+
+return (ptr);
+}
