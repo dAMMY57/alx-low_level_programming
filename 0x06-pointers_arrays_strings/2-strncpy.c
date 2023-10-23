dc1ca56836c8554713933cdc7411aec661bfd051
Odeyemi Damilola <pismart001@gmail.com>
2023-10-23 15:38:15 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+/**
+ * _strncpy - Copy a string (up to n bytes)
+ * @dest: The destination string
+ * @src: The source string
+ * @n: Maximum number of bytes to copy
+ *
+ * Return: Pointer to the resulting string (dest)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for (; i < n; i++)
+dest[i] = '\0';
+
+return (dest);
+}
